b95517b3d86ea4a21234a36e5a15e0896218e5f3
Christophe <Francis.Brown@jh.edu>
2021-05-21 20:55:39 -0400

    Yay

---
+++ 1.jl
@@ -0,0 +1,21 @@
+
+"""
+
+
+If we list all the natural numbers below 10 that are multiples of 
+3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+# Using a naive approach
+# Look for stupid 
+limit = 1000
+multiples = Int64[];
+# sizehint(multiples,limit)
+for i = 1:(limit-1)
+    if i%3 == 0 || i%5 == 0
+        push!(multiples,i)
+    end
+end
+
+println(size(multiples))
+println(sum(multiples))
